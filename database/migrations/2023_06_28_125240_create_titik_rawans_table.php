e0968aa6e8606de84a65804549562484b96fb529
lazlanrafar <lazlanrafar@gmail.com>
2023-06-29 03:51:23 +0700

    migration table titik rawan

--- database/migrations/2023_06_28_125240_create_titik_rawans_table.php
+++ database/migrations/2023_06_28_125240_create_titik_rawans_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('titik_rawans', function (Blueprint $table) {
             $table->id();
+            $table->string('kategori');
+            $table->string('nama_lokasi');
+            $table->string('koordinat_lokasi');
+            $table->string('foto_lokasi');
+            $table->string('informasi_bisnis');
+            $table->string('atensi_komoditas');
+            $table->string('riwayat_penindakan');
+            $table->string('riwayat_pelanggaran');
+            $table->string('tingkat_pelanggaran');
             $table->timestamps();
         });
     }
