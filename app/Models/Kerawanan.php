e5b44ed63873339580a1ecd0a18bdb6ba0f81058
lazlanrafar <lazlanrafar@gmail.com>
2023-07-01 02:53:25 +0700

    setup model kerawanan

--- app/Models/Kerawanan.php
+++ app/Models/Kerawanan.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Kerawanan extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'kategori',
+        'nama_lokasi',
+        'koordinat_lokasi',
+        'foto_lokasi',
+        'informasi_bisnis',
+        'atensi_komoditas',
+        'riwayat_penindakan',
+        'riwayat_pelanggaran',
+        'tingkat_pelanggaran',
+    ];
 }
--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -9,7 +9,7 @@ class Pegawai extends Model
 {
     use HasFactory;
 
-        /**
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
