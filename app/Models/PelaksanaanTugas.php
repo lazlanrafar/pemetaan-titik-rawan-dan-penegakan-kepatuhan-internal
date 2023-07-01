71d538e6f90ef971489138f4a99151efbe53ed3d
lazlanrafar <lazlanrafar@gmail.com>
2023-07-01 08:10:45 +0700

    setup model pelaksanaan tugas

--- app/Models/PelaksanaanTugas.php
+++ app/Models/PelaksanaanTugas.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class PelaksanaanTugas extends Model
 {
     use HasFactory;
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_petugas',
+        'nama_pelaksana',
+        'tanggal',
+        'jenis_waskat',
+        'tempat',
+        'tanggal_periode',
+        'lokasi_pelaksanaan_1',
+        'lokasi_pelaksanaan_2',
+        'lokasi_pelaksanaan_3',
+        'lokasi_pelaksanaan_4',
+        'hasil_pelaksanaan',
+        'kesimpulan',
+        'nama_pelanggar',
+        'foto_pelanggar',
+        'bidang_pelanggar',
+        'pelanggaran',
+        'dokumantasi_pelanggaran',
+        'informasi_tambahan',
+        'dokumentasi_1',
+        'dokumentasi_2',
+        'dokumentasi_3',
+        'dokumentasi_4',
+        'dokumentasi_5',
+    ];
 }
