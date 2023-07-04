427cf3698b5ff7a10144cc18b54acd67f76eb7ff
lazlanrafar <lazlanrafar@gmail.com>
2023-07-05 03:10:26 +0700

    setup model kegiatan internal

--- app/Models/KegiatanInternal.php
+++ app/Models/KegiatanInternal.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class KegiatanInternal extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_petugas',
+        'jenis_kegiatan',
+        'lokasi_kegiatan',
+    ];
+
+    function petugas(){
+         return $this->belongsTo(User::class, 'id_petugas');
+    }
 }
--- app/Models/KegiatanInternalDetail.php
+++ app/Models/KegiatanInternalDetail.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\KegiatanInternal;
+use App\Models\Pegawai;
 
 class KegiatanInternalDetail extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_kegiatan_internal',
+        'id_pegawai',
+        'is_kehadiran',
+        'is_pelanggaran',
+        'pelanggaran',
+        'is_penghargaan',
+        'penghargaan',
+    ];
+
+    function kegiatan_internal(){
+         return $this->belongsTo(KegiatanInternal::class, 'id_kegiatan_internal');
+    }
+
+    function pegawai(){
+         return $this->belongsTo(Pegawai::class, 'id_pegawai');
+    }
 }
