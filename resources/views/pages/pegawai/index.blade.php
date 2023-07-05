7356c4128d3f77626c3c7b05e0eb42ff972daa32
lazlanrafar <lazlanrafar@gmail.com>
2023-07-06 02:00:26 +0700

    refactor pegawai

--- resources/views/pages/pegawai/index.blade.php
+++ resources/views/pages/pegawai/index.blade.php
@@ -65,6 +65,11 @@
                                             </td>
                                             <td>
                                                 @if (request()->session()->get('user')['role'] === 'Pelaksana')
+                                                    <a type="button" class="btn btn-warning" data-toggle="modal"
+                                                        data-target="#formUpdatePerilaku{{ $item->id }}">
+                                                        Perilaku
+                                                    </a>
+                                                    @include('pages.pegawai.update-perilaku')
                                                     <form id="formDelete{{ $item->id }}"
                                                         action="{{ route('pegawai.destroy', $item->id) }}" method="POST"
                                                         class="d-inline">
--- resources/views/pages/pegawai/update-perilaku.blade.php
+++ resources/views/pages/pegawai/update-perilaku.blade.php
@@ -1,14 +1,14 @@
 <!-- Modal -->
-<div class="modal fade" id="formUpdate{{ $detail->id }}" tabindex="-1" role="dialog" aria-labelledby="formUpdateLabel"
-    aria-hidden="true">
+<div class="modal fade" id="formUpdatePerilaku{{ $item->id }}" tabindex="-1" role="dialog"
+    aria-labelledby="formUpdatePerilakuLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <form action="/pemantauan-kegiatan-internal-detail/{{ $detail->id }}" method="POST">
+            <form action="{{ route('pegawai.update', $item->id) }}" method="POST">
                 @csrf
                 @method('PUT')
                 <div class="modal-header">
-                    <h5 class="modal-title" id="formUpdateLabel">
-                        Update Pegawai
+                    <h5 class="modal-title" id="formUpdatePerilakuLabel">
+                        Update Pegawai - {{ $item->name }}
                     </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -20,30 +20,30 @@
                             <div class="form-group">
                                 <label>Pelanggaran</label>
                                 <select name="is_pelanggaran" class="form-control">
-                                    <option value="true" @if ($detail->is_pelanggaran) selected @endif>Ya</option>
-                                    <option value="false" @if (!$detail->is_pelanggaran) selected @endif>Tidak
+                                    <option value="true" @if ($item->is_pelanggaran) selected @endif>Ya</option>
+                                    <option value="false" @if (!$item->is_pelanggaran) selected @endif>Tidak
                                     </option>
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label>Pelanggaran yang Dilakukan</label>
                                 <input type="text" class="form-control" name="pelanggaran"
-                                    value="{{ $detail->pelanggaran }}" />
+                                    value="{{ $item->pelanggaran }}" />
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Penghargaan</label>
                                 <select name="is_penghargaan" class="form-control">
-                                    <option value="true" @if ($detail->is_penghargaan) selected @endif>Ya</option>
-                                    <option value="false" @if (!$detail->is_penghargaan) selected @endif>Tidak
+                                    <option value="true" @if ($item->is_penghargaan) selected @endif>Ya</option>
+                                    <option value="false" @if (!$item->is_penghargaan) selected @endif>Tidak
                                     </option>
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label>Penghargaan yang Dilakukan</label>
                                 <input type="text" class="form-control" name="penghargaan"
-                                    value="{{ $detail->penghargaan }}" />
+                                    value="{{ $item->penghargaan }}" />
                             </div>
                         </div>
                     </div>
