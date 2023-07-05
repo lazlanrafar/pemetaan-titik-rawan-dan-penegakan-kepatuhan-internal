d7af3a732193c775742afa42346908d011c4d991
lazlanrafar <lazlanrafar@gmail.com>
2023-07-06 00:22:16 +0700

    slicing dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -3,7 +3,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
+                    <h1 class="m-0">Selamat Datang {{ request()->session()->get('user')['name'] }} 👋</h1>
                 </div>
             </div>
         </div>
@@ -14,7 +14,16 @@
             <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class="col-12">
-
+                    <div class="card shadow-none">
+                        <img class="card-img-top" src="{{ url('/bg-login.jpg') }}" alt="Card image cap" height="300"
+                            style="object-fit: cover; object-position: top">
+                        <div class="card-body text-poppins">
+                            <p> Aplikasi Pemetaan Titik Rawan merupakan aplikasi yang dikembangkan oleh Bidang Kepatuhan
+                                Internal yang memuat data pemetaan titik rawan integritas, hasil pendampingan pemeriksaan
+                                fisik,
+                                hasil kegiatan pengendalian internal dan profiling pegawai.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
